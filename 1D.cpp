ca55d898b92f957a4b0bb745959e7d460abf106a
galloska <skap19952008@hotmail.com>
2015-09-13 00:17:02 -0500

    Create 1D.cpp

---
+++ 1D.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    freopen("yield.in","r",stdin);
+    freopen("yield.out","w",stdout);
+    double a,b;
+    scanf("%lf %lf",&a,&b);
+    printf("%.4lf\n",a+b);
+    return 0;
+}
